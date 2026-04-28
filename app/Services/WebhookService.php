1bed8868be49cf5a8d99e0e2fbafe0f4e1eee298
Martin Rener <martinrener04@gmail.com>
2026-04-28 16:50:40 -0300

    webhookService added

---
+++ app/Services/WebhookService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\EventLogRepositoryInterface;
+use App\Contracts\PaymentRepositoryInterface;
+use App\DTOs\EventLogDto;
+use App\DTOs\PaymentDto;
+
+class WebhookService
+{
+    public function __construct(
+        private EventLogRepositoryInterface $event_log_repo,
+        private PaymentRepositoryInterface $payment_repo,
+    ){}
+
+    public function receivePayment(EventLogDto $event): void 
+    {
+        $event_already_exists = $this->event_log_repo->existsEvent($event->event_id);
+        $this->event_log_repo->store($event);
+        if(!$event_already_exists){
+            try{
+                $new_payment_details = $this->buildPaymentDto($event);
+                $this->payment_repo->upsert($new_payment_details);
+            }catch(\Exception $e){
+                \Log::error('Error upserting payment: ' . $e->getMessage());
+            }
+        }
+    }
+    private function buildPaymentDto(EventLogDto $event): PaymentDto
+    {
+        return new PaymentDto(
+            payment_id: $event->payment_id,
+            event: $event->event,
+            currency: $event->currency,
+            amount: $event->amount,
+            user_id: $event->user_id,
+            last_event_id: $event->event_id,
+        );
+    }
+}
