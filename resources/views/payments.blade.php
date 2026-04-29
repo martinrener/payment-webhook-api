d057caa92c33094890622fdb00eb470ed3aa8c60
Martin Rener <martinrener04@gmail.com>
2026-04-29 16:04:49 -0300

    payment.blade.php added

---
+++ resources/views/payments.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Payments</title>
+    </head>
+    <body>
+        <h1>Payments</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>payment_id</th>
+                    <th>event</th>
+                    <th>currency</th>
+                    <th>user_id</th>
+                    <th>last_event_id</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($payments as $payment)
+                    <tr>
+                        <td>{{ $payment->payment_id }}</td>
+                        <td>{{ $payment->event }}</td>
+                        <td>{{ $payment->currency }}</td>
+                        <td>{{ $payment->user_id }}</td>
+                        <td>{{ $payment->last_event_id }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </body>
+</html>
