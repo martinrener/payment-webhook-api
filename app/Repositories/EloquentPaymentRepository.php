3f1988820c22a07fa49804fa35add37fe3305594
Martin Rener <martinrener04@gmail.com>
2026-05-19 14:13:30 -0300

    fix: query duplicated logic unifided

--- app/Repositories/EloquentPaymentRepository.php
+++ app/Repositories/EloquentPaymentRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Models\Payment;
 use App\DTOs\PaymentDto;
 use App\Contracts\PaymentRepositoryInterface;
+use Illuminate\Database\Eloquent\Builder;
 
 class EloquentPaymentRepository implements PaymentRepositoryInterface 
 {
@@ -26,9 +27,11 @@ class EloquentPaymentRepository implements PaymentRepositoryInterface
     {
         return Payment::where('payment_id', $paymentId)->first();
     }
-    public function list(int $page = 1,int $perPage = 10, ?string $event = null, ?string $user_id = null, ?string $currency = null, ?string $dateFrom = null, ?string $dateTo = null): array
+
+    private function buildFilteredQuery(?string $event, ?string $user_id, ?string $currency, ?string $dateFrom, 
+                                        ?string $dateTo): Builder
     {
-        return Payment::when($event, function ($query) use ($event) {
+        return Payment::query()->when($event, function ($query) use ($event) {
                 $query->where('event', $event);
             })
             ->when($user_id, function ($query) use ($user_id) {
@@ -42,7 +45,13 @@ class EloquentPaymentRepository implements PaymentRepositoryInterface
             })
             ->when($dateTo, function ($query) use ($dateTo) {
                 $query->whereDate('created_at', '<=', $dateTo);
-            })
+            });
+    }
+
+    public function list(int $page = 1,int $perPage = 10, ?string $event = null, ?string $user_id = null, 
+                        ?string $currency = null, ?string $dateFrom = null, ?string $dateTo = null): array
+    {
+        return $this->buildFilteredQuery($event, $user_id, $currency, $dateFrom, $dateTo)
             ->orderBy('created_at', 'desc')
             ->paginate($perPage, ['*'], 'page', $page)
             ->toArray();
@@ -50,21 +59,7 @@ class EloquentPaymentRepository implements PaymentRepositoryInterface
 
     public function export(?string $event = null, ?string $user_id = null, ?string $currency = null, ?string $dateFrom = null, ?string $dateTo = null): array
     {
-        return Payment::when($event, function ($query) use ($event) {
-                $query->where('event', $event);
-            })
-            ->when($user_id, function ($query) use ($user_id) {
-                $query->where('user_id', $user_id);
-            })
-            ->when($currency, function ($query) use ($currency) {
-                $query->where('currency', $currency);
-            })
-            ->when($dateFrom, function ($query) use ($dateFrom) {
-                $query->whereDate('created_at', '>=', $dateFrom);
-            })
-            ->when($dateTo, function ($query) use ($dateTo) {
-                $query->whereDate('created_at', '<=', $dateTo);
-            })
+        return $this->buildFilteredQuery( $event, $user_id, $currency, $dateFrom, $dateTo)
             ->orderBy('created_at', 'desc')
             ->get()
             ->toArray();
